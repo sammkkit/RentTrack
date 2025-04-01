94c53c942cf5322b92df2fc1fb4c782a2e7e4d02
sammkkit <samkitjain430@gmail.com>
2025-04-01 21:45:49 +0530

    deploying notification feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.samapp.renttrack"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "2"
+        versionCode = 11
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
