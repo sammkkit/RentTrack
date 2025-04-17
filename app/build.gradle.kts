edbd417606e94c3d3637d53ad0ce5f450c19c420
sammkkit <samkitjain430@gmail.com>
2025-04-17 23:10:03 +0530

    new aab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.samapp.renttrack"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10
-        versionName = "2"
+        versionCode = 11
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/samapp/renttrack/util/FirebaseNotification.kt
+++ app/src/main/java/com/samapp/renttrack/util/FirebaseNotification.kt
@@ -1,11 +1,14 @@
 package com.samapp.renttrack.util
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.samapp.renttrack.MainActivity
 import com.samapp.renttrack.R
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
@@ -17,11 +20,22 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     private fun showNotification(title: String?, message: String?) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this,0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
         val builder = NotificationCompat.Builder(this, "channel_id")
             .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(title)
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(0, builder.build())
